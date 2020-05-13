e02a8241d9645ea94e4a9c3760466b7aa1984f0b
jsfiki <juanfiquitiva@hotmail.com>
2020-05-13 01:46:34 +0000

    codigo

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('ejercicio exitoso');
 }).listen(8080);
 
